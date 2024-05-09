7ef64b3e23d307070247f9574da468990ed0a7af
Mohaned_omer <mohanedomerhamed001234@gmail.com>
2024-05-09 03:56:57 +0300

    save

--- fezzbuz.js
+++ fezzbuz.js
@@ -1,22 +1,29 @@
-const Fezz = function (start,end){
-    let i = start ;
-    let arr = [];
-    function say(theArray,theCounter){
-        if (theArray.length === 0){
-            return theCounter
-        }
-        return ''
+const Fezz = function (start, end) {
+  let i = start;
+  let arr = [];
+  function say(theArray, theCounter) {
+    if (theArray.length === 0) {
+      return theCounter;
     }
-    while(i<end){
-        
-        if(i%3===0){arr.push('fuzz')} ;
-        if(i%5===0){arr.push('buzz')} ;
-        if(i%7===0){arr.push('foo')} ;
-        if(i%11===0){arr.push('bo')} ;
-        console.log( say(arr,i) , arr.join(''))
-        i++;
-        arr = [] ;
+    return "";
+  }
+  while (i < end) {
+    if (i % 3 === 0) {
+      arr.push("fuzz");
     }
-    return arr
-}
-Fezz(0,200) ;
+    if (i % 5 === 0) {
+      arr.push("buzz");
+    }
+    if (i % 7 === 0) {
+      arr.push("foo");
+    }
+    if (i % 11 === 0) {
+      arr.push("bo");
+    }
+    console.log(say(arr, i), arr.join(""));
+    i++;
+    arr = [];
+  }
+  return arr;
+};
+Fezz(0, 200);
